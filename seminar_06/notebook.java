5b12aa4e71066da2809c5225984df1f69502323e
Виктор Петкевич <torrvick@gmail.com>
2023-03-21 16:32:37 +0300

    Changes in seminar_06

--- seminar_06/notebook.java
+++ seminar_06/notebook.java
@@ -1,5 +1,7 @@
 package seminar_06;
 
+import java.util.Random;
+
 public class notebook {
     private String manufacture;
     private String model;
@@ -9,5 +11,117 @@ public class notebook {
     private int storage;
     private String os;
     private double weight;
+    private String color;
+
+
+    public notebook() {
+        setRandColor();
+    }
+
+    public notebook(String manufacture, String model, double screenSize, String cpu, int ram, int storage, String os, double weight, String color) {
+        this.manufacture = manufacture;
+        this.model = model;
+        this.screenSize = screenSize;
+        this.cpu = cpu;
+        this.ram = ram;
+        this.storage = storage;
+        this.os = os;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public String getManufacture() {
+        return this.manufacture;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getScreenSize() {
+        return this.screenSize;
+    }
+
+    public void setScreenSize(double screenSize) {
+        this.screenSize = screenSize;
+    }
+
+    public String getCpu() {
+        return this.cpu;
+    }
+
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    public int getRam() {
+        return this.ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getStorage() {
+        return this.storage;
+    }
+
+    public void setStorage(int storage) {
+        this.storage = storage;
+    }
+
+    public String getOs() {
+        return this.os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    private void setRandColor() {
+        String[] colors = {"Белый", "Серый", "Черный"};
+        this.color = colors[new Random().nextInt(3)];
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " manufacture='" + getManufacture() + "'" +
+            ", model='" + getModel() + "'" +
+            ", screenSize='" + getScreenSize() + "'" +
+            ", cpu='" + getCpu() + "'" +
+            ", ram='" + getRam() + "'" +
+            ", storage='" + getStorage() + "'" +
+            ", os='" + getOs() + "'" +
+            ", weight='" + getWeight() + "'" +
+            ", color='" + getColor() + "'" +
+            "}";
+    }
+
+    
 
 }
--- seminar_06/notebookFinder.java
+++ seminar_06/notebookFinder.java
@@ -2,14 +2,49 @@ package seminar_06;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class notebookFinder {
     public static void main(String[] args) throws Exception {
-        String workDir = System.getProperty("user.dir");
-        BufferedReader br = new BufferedReader(new FileReader(workDir.concat("/seminar_06/laptops.csv"))) ;
-        String s;
-        while((s=br.readLine())!=null){    
-            System.out.println(s);
+        List<notebook> notebooks = getFromCSV("/seminar_06/laptops.csv");
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        for (notebook elem : notebooks) {
+            if (elem.getColor().equals("Белый") && elem.getStorage() > 1000) System.out.println(elem);
         }
     }
+
+    static List<notebook> getFromCSV(String fileName) throws Exception{
+        String workDir = System.getProperty("user.dir");
+        BufferedReader br = new BufferedReader(new FileReader(workDir.concat(fileName))) ;
+        String noteLine;
+        List<notebook> notebooks = new ArrayList<>();
+        while((noteLine=br.readLine())!=null){  
+            String noteArr[] = noteLine.split(";"); 
+
+            String manufacture = noteArr[0];
+            String model = noteArr[1];
+            double screenSize = Double.parseDouble(noteArr[2]);
+            String cpu = noteArr[3];
+            int ram = Integer.parseInt(noteArr[4]);
+            int storage = Integer.parseInt(noteArr[5]);
+            String os = noteArr[6];
+            double weight = Double.parseDouble(noteArr[7]);
+
+            notebook nb = new notebook();
+            nb.setManufacture(manufacture);
+            nb.setModel(model);
+            nb.setScreenSize(screenSize);
+            nb.setCpu(cpu);
+            nb.setRam(ram);
+            nb.setStorage(storage);
+            nb.setOs(os);
+            nb.setWeight(weight);
+
+        notebooks.add(nb);
+        }
+        br.close();
+        return notebooks;
+    }
+
 }
