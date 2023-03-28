70de814af7fd7eacb543d420c2f442addf67704c
Vit <torrvick@gmail.com>
2023-03-28 22:28:00 +0300

    Changed to Hash

--- coffeeMachine/CoffeeMachine.java
+++ coffeeMachine/CoffeeMachine.java
@@ -1,25 +1,36 @@
 package coffeeMachine;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CoffeeMachine {
-    private List<Coffee> reciepsList;
+    private HashMap<Integer, Coffee> reciepsList;
     private double balance;
+
+    
     private int totalWater;
     private int totalMilk;
+    private int id;
     private boolean maitenanceMode;
 
     public CoffeeMachine() {
-        this.reciepsList = new ArrayList<Coffee>();
+        this.reciepsList = new HashMap<Integer, Coffee>();
         this.balance = 0;
         this.totalWater = 1000;
         this.totalMilk = 1000;
+        this.id = 0;
         this.maitenanceMode = false;
     }
     public CoffeeMachine addReceipe(Coffee receipe) {
-        reciepsList.add(receipe);
+        // reciepsList.add(receipe);
+        reciepsList.put(++id, receipe);
         return this;
+    }
+
+    public double getBalance() {
+        return this.balance;
     }
 
     public void insertCoin(double coin) {
@@ -30,23 +41,24 @@ public class CoffeeMachine {
         return value/4*ratio;
     }
 
-    public List<Coffee> getList() {
+    public HashMap<Integer, Coffee> getList() {
         return this.reciepsList;
     }
 
     public void sellCoffee(String name) {
         if (!maitenanceMode){
-            for (Coffee coffee : reciepsList) {
-                if (coffee.getName().equals(name)) {
-                    if (this.balance > coffee.getPrice()) {
-                        System.out.println("Вы купили " + coffee.getName());
-                        this.balance -= coffee.getPrice();
+            // for (Coffee coffee : reciepsList) {
+            for (HashMap.Entry<Integer, Coffee> coffee: reciepsList.entrySet()){
+                if (coffee.getValue().getName().equals(name)) {
+                    if (this.balance > coffee.getValue().getPrice()) {
+                        System.out.println("Вы купили " + coffee.getValue().getName());
+                        this.balance -= coffee.getValue().getPrice();
                         if (coffee instanceof MilkCoffee) {
                             MilkCoffee item = (MilkCoffee)coffee;
                             this.totalWater -= item.getVolume() - milkVolume(item.getVolume(),item.getMilkRatio());
                             this.totalMilk -= milkVolume(item.getVolume(),item.getMilkRatio());
                         } else {
-                            this.totalWater -= coffee.getVolume();
+                            this.totalWater -= coffee.getValue().getVolume();
                         }
 
                         if (this.totalWater < 500 || this.totalMilk < 500) {
@@ -65,8 +77,11 @@ public class CoffeeMachine {
     @Override
     public String toString() {
         StringBuilder outputString = new StringBuilder();
-        for (Coffee receipe : reciepsList) {
-            outputString.append(receipe);
+        // for (Coffee receipe : reciepsList) {
+        for (HashMap.Entry<Integer, Coffee> receipe: reciepsList.entrySet()){
+            outputString.append(receipe.getKey());
+            outputString.append(": ");
+            outputString.append(receipe.getValue());
             outputString.append("\n");
         }
         return outputString.toString();
--- coffeeMachine/Main.java
+++ coffeeMachine/Main.java
@@ -1,9 +1,11 @@
 package coffeeMachine;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         CoffeeMachine machine = new CoffeeMachine();
         machine.addReceipe(new Coffee("Американо большой", 400, 90, 100))
                 .addReceipe(new Coffee("Американо маленький", 200, 90, 80))
@@ -17,11 +19,29 @@ public class Main {
         // System.out.println();
         // machine.insertCoin(400);
         // machine.sellCoffee("Маккиато большой");
-        List<Coffee> coffeeList = machine.getList();
-        for (Coffee item : coffeeList) {
-            System.out.println(item);
+        HashMap<Integer, Coffee> coffeeList = machine.getList();
+        System.out.println("В наличии есть следующие напитки: \n");
+        StringBuilder outputString = new StringBuilder();
+        for (HashMap.Entry<Integer, Coffee> item: coffeeList.entrySet()){
+            // System.out.printf("%s: %-20s %5sмл - %6s р.\n", item.getKey(), item.getValue().getName(), item.getValue().getVolume(), item.getValue().getPrice());
+            outputString.append(String.format("%s: %-20s %5sмл - %6s р.\n", item.getKey(), item.getValue().getName(), item.getValue().getVolume(), item.getValue().getPrice()));
         }
+        System.out.println(outputString);
+        System.out.println();
+        System.out.printf("Текущий баланс: %s\n", machine.getBalance());
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите количество монет: ");
+        int coins = in.nextInt();
+        machine.insertCoin(coins);
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        System.out.println(outputString);
+        System.out.println();
+        System.out.printf("Текущий баланс: %s", machine.getBalance());
 
+        // System.out.println(coffeeList.get(2));
+    }
 
+    private static void mainMenu() {
+        
     }
 }
