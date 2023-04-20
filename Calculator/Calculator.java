db90892b56913e39b0819369ba816dad4c968c07
Виктор Петкевич <torrvick@gmail.com>
2023-04-20 14:20:25 +0300

    Init Calc

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -8,29 +8,6 @@ public final class Calculator implements Calculable {
         this.primaryArg = primaryArg;
     }
 
-    private static ComplexNumber parseComplexNumber(String complex) {
-        int plusIndex = complex.indexOf("+");
-        int minusIndex = complex.indexOf("-");
-        int iIndex = complex.indexOf("i");
-
-        int realPart;
-        int imaginaryPart;
-
-        if (plusIndex != -1) {
-            realPart = Integer.parseInt(complex.substring(0, plusIndex));
-            imaginaryPart = Integer.parseInt(complex.substring(plusIndex + 1, iIndex));
-        } else if (minusIndex != -1) {
-            realPart = Integer.parseInt(complex.substring(0, minusIndex));
-            imaginaryPart = Integer.parseInt(complex.substring(minusIndex, iIndex));
-        } else {
-            realPart = 0;
-            imaginaryPart = Integer.parseInt(complex.substring(0, iIndex));
-        }
-
-        return new ComplexNumber(realPart, imaginaryPart);
-    }
-
-
     @Override
     public Calculable add(ComplexNumber arg) {
         primaryArg.add(arg);
--- Calculator/ViewCalculator.java
+++ Calculator/ViewCalculator.java
@@ -46,11 +46,11 @@ public class ViewCalculator {
         return in.nextLine();
     }
 
-    private int promptInt(String message) {
-        Scanner in = new Scanner(System.in);
-        System.out.print(message);
-        return Integer.parseInt(in.nextLine());
-    }
+    // private int promptInt(String message) {
+    //     Scanner in = new Scanner(System.in);
+    //     System.out.print(message);
+    //     return Integer.parseInt(in.nextLine());
+    // }
 
     private ComplexNumber promptComplex(String message) {
         Scanner in = new Scanner(System.in);
